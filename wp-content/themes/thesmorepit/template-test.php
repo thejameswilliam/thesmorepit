824b559fa08cf448f4bd030162f71e89e606dd97
James Johnson <James@Jamess-MacBook-Pro.local>
2016-12-12 20:35:43 -0700

    small tweaks.

--- wp-content/themes/thesmorepit/template-test.php
+++ wp-content/themes/thesmorepit/template-test.php
@@ -1,5 +1,10 @@
 <?php /* Template Name: Test Template */ ?>
 <?php get_header(); ?>
+
+	<main role="main" class="page">
+		<div class="top-logo screen fullheight red_background">
+
+			<section class="container col-md-10 col-md-offset-1">
 				<?php
 				$image_args = array(
 					'w'     => 1050,
@@ -12,17 +17,22 @@
 
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
+			<!-- article -->
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 				<?php the_content(); ?>
 				<br class="clear">
 
 				<?php smore_edit_link(); ?>
 
+			</article>
 			<!-- /article -->
 
 		<?php endwhile; endif; ?>
 
 		<!-- /section -->
-
+	</div>
+	</section>
+	</main>
 
 <?php get_footer(); ?>
