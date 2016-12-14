2560c4ed45fcf19e212d7ae57ea8849c4bb7aac9
James Johnson <James@Jamess-MacBook-Pro.local>
2016-12-13 22:24:26 -0700

    updated look of search form.

--- wp-content/themes/thesmorepit/search.php
+++ wp-content/themes/thesmorepit/search.php
@@ -4,11 +4,11 @@
 	<div class="top-logo screen light_background">
 
 		<section class="container fullheight col-md-8 col-md-offset-2">
-			<?php get_template_part('searchform'); ?>
+
 			<div class="col-md-12">
 				<h1><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
 			</div>
-
+			<?php get_template_part('searchform'); ?>
 			<?php get_template_part('loop'); ?>
 
 			<?php get_template_part('pagination'); ?>
--- wp-content/themes/thesmorepit/searchform.php
+++ wp-content/themes/thesmorepit/searchform.php
@@ -1,7 +1,7 @@
-<div class="col-md-12 aligncenter">
+<div class="col-md-8 col-md-offset-2">
 <!-- search -->
 <form class="form-inline" method="get" action="<?php echo home_url(); ?>" role="search">
-	<div class="input-group">
+	<div class="input-group input-group-lg col-md-12">
 		<input class="form-control" type="search" name="s" placeholder="Search here">
 		<span class="input-group-btn">
 			<button class="search-submit btn btn-default" type="submit" role="button"><?php _e( 'Search', 'html5blank' ); ?></button>
