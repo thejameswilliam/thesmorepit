5bfb430f1807d1ce10b1c660afcc30f0d2fd443e
James Johnson <James@Jamess-MacBook-Pro.local>
2016-12-27 18:58:44 -0700

    removed sidebar from woo pages.

--- wp-content/themes/thesmorepit/functions.php
+++ wp-content/themes/thesmorepit/functions.php
@@ -469,6 +469,13 @@ function woocommerce_support() {
     add_theme_support( 'woocommerce' );
 }
 
+add_action( 'get_header', 'remove_storefront_sidebar' );
+function remove_storefront_sidebar() {
+	if ( is_woocommerce() ) {
+		remove_action( 'storefront_sidebar', 'storefront_get_sidebar', 10 );
+	}
+}
+
 /*------------------------------------*\
 	Options Page
 \*------------------------------------*/
