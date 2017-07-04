b35ac8e8685b98d0647b97124f7b92778fcd3ed4
James Johnson <James@Jamess-MacBook-Pro.local>
2017-07-03 18:53:04 -0600

    re-arranging notes.

--- wp-content/themes/thesmorepit/index.php
+++ wp-content/themes/thesmorepit/index.php
@@ -1,83 +1,82 @@
 <?php get_header(); ?>
 
-	<main id="main" role="main">
+<main id="main" role="main">
 
 
-		<div class="top-logo screen light_background">
-			<div class="fullheight ">
-				<div class="logo col-md-6 col-md-offset-3">
-					<?php
-					$img_src = array(
-						'src' => get_template_directory_uri() . '/img/logo.png',
-						'w'   => 600,
+	<div class="top-logo screen light_background">
+		<div class="fullheight ">
+			<div class="logo col-md-6 col-md-offset-3">
+				<?php
+				$img_src = array(
+					'src' => get_template_directory_uri() . '/img/logo.png',
+					'w'   => 600,
 
-					); ?>
+				); ?>
 
-					<img src="<?php echo mapi_thumb($img_src); ?>" alt="The S'more Pit">
-				</div>
+				<img src="<?php echo mapi_thumb($img_src); ?>" alt="The S'more Pit">
+			</div>
 
-				<div class="social_icons col-md-4 col-md-offset-4">
-					<?php
-					$networks = array(
-						'facebook',
-						'twitter',
-						'instagram',
-					);
-					mapi_social_links($networks); ?>
-				</div>
+			<div class="social_icons col-md-4 col-md-offset-4">
+				<?php
+				$networks = array(
+					'facebook',
+					'twitter',
+					'instagram',
+				);
+				mapi_social_links($networks); ?>
 			</div>
 		</div>
+	</div>
 
 
 
-		<?php if(get_field('content', 'options')) : ?>
-			<div class="red_background screen">
-				<div class="container col-md-8 col-md-offset-2">
-					<?php the_field('content', 'options'); ?>
-				</div>
+	<?php if(get_field('content', 'options')) : ?>
+		<div class="red_background screen">
+			<div class="container col-md-8 col-md-offset-2">
+				<?php the_field('content', 'options'); ?>
 			</div>
-		<?php endif; ?>
+		</div>
+	<?php endif; ?>
+	
 
-
-		<div class="red_background screen ">
-			<div class="container fullheight col-md-8 col-md-offset-2">
-			<?php
-
-			// check if the repeater field has rows of data
-			if( have_rows('home_page_images', 'options') ):
-				echo '<div class="row">';
-    		while ( have_rows('home_page_images', 'options') ) : the_row();
-        $image = get_sub_field('image');
+	<?php
+	// check if the repeater field has rows of data
+	if( have_rows('home_page_images', 'options') ): ?>
+	<div class="red_background screen ">
+		<div class="container col-md-8 col-md-offset-2">
+			<div class="row">'
+				<?php
+				while ( have_rows('home_page_images', 'options') ) : the_row();
+				$image = get_sub_field('image');
 				$img_url = mapi_thumb($image['url'], 400, 280, 90);
 				?>
-					<div class="col-xs-4">
-						<img src="<?php echo $img_url; ?>" alt="<?php echo $image['title']; ?>" />
-					</div>
+				<div class="col-xs-4">
+					<img src="<?php echo $img_url; ?>" alt="<?php echo $image['title']; ?>" />
+				</div>
 
-    		<?php endwhile;
-				echo '</div>';
-				endif;
-					?>
+			<?php endwhile;
+			echo '</div>';
+			echo '</div>';
+			echo '</div>';
+		endif;
+		?>
+
+
+		<div class="light_background screen">
+			<div class="container col-md-8 col-md-offset-2">
+				<h1>Featured S'Mores</h1>
+				<p align="center"><a class="btn btn-primary" href="/the-smore-shoppe/">The S'More Shoppe</a></p>
+
+				<div class="col-sm-12">
+					<p align="center"><small>*Delivery available for Santa Fe and select surrounding areas. New Mexico customers, please arrange for pick-up.</small></p>
+				</div>
+				<div class="col-sm-12">
+					<?php
+					echo apply_filters( 'the_content','[featured_products per_page="4" columns="4"]');
+					//do_shortcode('[featured_products per_page="4" columns="4"]'); ?>
+				</div>
 			</div>
 		</div>
-
-
-
-						<div class="light_background screen">
-							<div class="container col-md-8 col-md-offset-2">
-									<h1>Featured S'Mores</h1>
-									<p align="center"><a class="btn btn-primary" href="/the-smore-shoppe/">The S'More Shoppe</a></p>
-
-									<div class="col-sm-12">
-										<p align="center"><small>*Delivery available for Santa Fe and select surrounding areas. New Mexico customers, please arrange for pick-up.</small></p>
-									</div>
-									<div class="col-sm-12">
-									<?php
-									echo apply_filters( 'the_content','[featured_products per_page="4" columns="4"]');
-									//do_shortcode('[featured_products per_page="4" columns="4"]'); ?>
-									</div>
-							</div>
-						</div>
 
 
 
@@ -92,4 +91,4 @@
 
 
 	</main>
-<?php get_template_part('footer'); ?>
+	<?php get_template_part('footer'); ?>
