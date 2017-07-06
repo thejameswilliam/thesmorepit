ee5d66d0a8a8302de6cdfb75db5a7b45228952c4
James Johnson <james@mindsharelabs.com>
2017-07-06 08:52:08 -0600

    added single featured smore to home page.

--- wp-content/themes/thesmorepit/index.php
+++ wp-content/themes/thesmorepit/index.php
@@ -3,91 +3,111 @@
 <main id="main" role="main">
 
 
-	<div class="top-logo screen light_background">
-		<div class="fullheight ">
-			<div class="logo col-md-6 col-md-offset-3">
-				<?php
-				$img_src = array(
-					'src' => get_template_directory_uri() . '/img/logo.png',
-					'w'   => 600,
+    <div class="top-logo screen light_background">
+        <div class="fullheight ">
+            <div class="logo col-md-6 col-md-offset-3">
+                <?php
+                $img_src = array(
+                    'src' => get_template_directory_uri() . '/img/logo.png',
+                    'w' => 600,
 
-				); ?>
+                ); ?>
 
-				<img src="<?php echo mapi_thumb($img_src); ?>" alt="The S'more Pit">
-			</div>
+                <img src="<?php echo mapi_thumb($img_src); ?>" alt="The S'more Pit">
+            </div>
 
-			<div class="social_icons col-md-4 col-md-offset-4">
-				<?php
-				$networks = array(
-					'facebook',
-					'twitter',
-					'instagram',
-				);
-				mapi_social_links($networks, 'follow'); ?>
-			</div>
-		</div>
-	</div>
-
-
-
-	<?php if(get_field('content', 'options')) : ?>
-		<div class="red_background screen">
-			<div class="container col-md-8 col-md-offset-2">
-				<?php the_field('content', 'options'); ?>
-			</div>
-		</div>
-	<?php endif; ?>
+            <div class="social_icons col-md-4 col-md-offset-4">
+                <?php
+                $networks = array(
+                    'facebook',
+                    'twitter',
+                    'instagram',
+                );
+                mapi_social_links($networks, 'follow'); ?>
+            </div>
+        </div>
+    </div>
 
 
-	<?php
-	// check if the repeater field has rows of data
-	if( have_rows('front_page_images', 'options') ): ?>
-	<div class="dark-background screen ">
-		<div class="container front-images col-md-8 col-md-offset-2">
-			<div class="row">
-				<?php
-				while ( have_rows('front_page_images', 'options') ) : the_row();
-				$image = get_sub_field('image');
-				$img_url = mapi_thumb($image['url'], 400, 280, 90);
-				?>
-				<div class="col-xs-4 front-image">
-					<img src="<?php echo $img_url; ?>" alt="<?php echo $image['title']; ?>" />
-				</div>
-
-			<?php endwhile; ?>
-		</div>
-	</div>
-</div>
-<?php endif;
-?>
+    <?php if (get_field('content', 'options')) : ?>
+        <div class="red_background screen">
+            <div class="container col-md-8 col-md-offset-2">
+                <?php the_field('content', 'options'); ?>
+            </div>
+        </div>
+    <?php endif; ?>
 
 
-<div class="light_background screen">
-	<div class="container col-md-8 col-md-offset-2">
-		<h1>Featured S'Mores</h1>
-		<p align="center"><a class="btn btn-primary" href="/the-smore-shoppe/">The S'More Shoppe</a></p>
+    <?php
+    // check if the repeater field has rows of data
+    if (have_rows('front_page_images', 'options')): ?>
+        <div class="dark-background screen ">
+            <div class="container front-images col-md-8 col-md-offset-2">
+                <div class="row">
+                    <?php
+                    while (have_rows('front_page_images', 'options')) : the_row();
+                        $image = get_sub_field('image');
+                        $img_url = mapi_thumb($image['url'], 400, 280, 90);
+                        ?>
+                        <div class="col-xs-4 front-image">
+                            <img src="<?php echo $img_url; ?>" alt="<?php echo $image['title']; ?>"/>
+                        </div>
 
-		<div class="col-sm-12">
-			<p align="center"><small>*Delivery available for Santa Fe and select surrounding areas. New Mexico customers, please arrange for pick-up.</small></p>
-		</div>
-		<div class="col-sm-12">
-			<?php
-			echo apply_filters( 'the_content','[featured_products per_page="4" columns="4"]');
-			//do_shortcode('[featured_products per_page="4" columns="4"]'); ?>
-		</div>
-	</div>
-</div>
+                    <?php endwhile; ?>
+                </div>
+            </div>
+        </div>
+    <?php endif;
+    ?>
 
 
+    <div class="light_background screen">
+        <div class="container col-md-8 col-md-offset-2">
+            <p align="center"><a class="btn btn-primary btn-lg" href="/the-smore-shoppe/">The S'More Shoppe</a></p>
+            <hr />
+            <h1>Featured S'More</h1>
+            <div class="col-sm-12">
+                <p align="center">
+                    <small>*Delivery available for Santa Fe and select surrounding areas. New Mexico customers, please
+                        arrange for pick-up.
+                    </small>
+                </p>
+            </div>
+            <?php if(get_field('product_id', 'options')) : ?>
+            <div class="col-sm-12">
+                <div class="row">
+                    <?php
+                    do_action( 'woocommerce_before_single_product' );
+                    $prod_id = get_field('product_id', 'options');
+                    $featured_prod = wc_get_product($prod_id);
+                    $image = $featured_prod->get_image('shop_thumbnail');
+                    $short_desc = $featured_prod->get_short_description();
+                    $title = $featured_prod->get_title();
+                    $price = $featured_prod->get_price();
+                    $link_text = $featured_prod->add_to_cart_text();
+                    $link_url = $featured_prod->add_to_cart_url();
+                    $prod_url = $featured_prod->get_permalink();
+                    ?>
+                    <div class="col-sm-3"><?php echo $image; ?></div>
+                    <div class="col-sm-9">
+                        <h2><a href="<?php echo $prod_url; ?>" title="<?php echo $title; ?>"><?php echo $title; ?></a>
+                        </h2>
+                        <p><?php echo $short_desc; ?></p>
+                        <a class="btn btn-primary pull-right" href="<?php echo $link_url; ?>"><?php echo $link_text; ?></a>
+                    </div>
+                </div>
+            </div>
+            <?php endif; ?>
+        </div>
+    </div>
 
 
-
-<div class="red_background screen ">
-	<div class="container fullheight col-md-8 col-md-offset-2">
-		<h1>Instagram</h1>
-		<?php echo do_shortcode('[instagram-feed]'); ?>
-	</div>
-</div>
+    <div class="red_background screen ">
+        <div class="container fullheight col-md-8 col-md-offset-2">
+            <h1>Instagram</h1>
+            <?php echo do_shortcode('[instagram-feed]'); ?>
+        </div>
+    </div>
 
 
 </main>
--- wp-content/themes/thesmorepit/nav.php
+++ wp-content/themes/thesmorepit/nav.php
@@ -1,34 +1,36 @@
 <nav class="nav" role="navigation">
-  <input type="checkbox" id="menuToggle">
-  <label for="menuToggle" class="menu-icon"><i class="fa fa-bars" aria-hidden="true"></i></label>
+    <input type="checkbox" id="menuToggle">
+    <label for="menuToggle" class="menu-icon"><i class="fa fa-bars" aria-hidden="true"></i></label>
 
 
-  <nav class="menu">
-    <?php
-    $img_src = array(
-      'src' => get_template_directory_uri() . '/img/smores_white.png',
-      'w'   => 200,
+    <nav class="menu">
+        <div class="logo">
+            <?php
+            $img_src = array(
+                'src' => get_template_directory_uri() . '/img/smores_white.png',
+                'w' => 200,
 
-    ); ?>
+            ); ?>
 
-    <a href="<?php echo site_url(); ?>"><img src="<?php echo mapi_thumb($img_src); ?>" alt="The S'more Pit"></a>
-    <div class="col-md-12">
-    <!-- search -->
-    <form class="form-inline" method="get" action="<?php echo home_url(); ?>" role="search">
-    	<div class="input-group">
-    		<input class="form-control" type="search" name="s" placeholder="Search here">
-    		<span class="input-group-btn">
-    			<button class="search-submit btn btn-default" type="submit" role="button"><?php _e( 'Search', 'html5blank' ); ?></button>
+            <a href="<?php echo site_url(); ?>"><img src="<?php echo mapi_thumb($img_src); ?>" alt="The S'more Pit"></a>
+        </div>
+        <div class="col-md-12">
+            <!-- search -->
+            <form class="form-inline" method="get" action="<?php echo home_url(); ?>" role="search">
+                <div class="input-group">
+                    <input class="form-control" type="search" name="s" placeholder="Search here">
+                    <span class="input-group-btn">
+    			<button class="search-submit btn btn-default" type="submit"
+                        role="button"><?php _e('Search', 'html5blank'); ?></button>
     		</span>
-    	</div>
-    </form>
-    </div>
+                </div>
+            </form>
+        </div>
 
 
+        <br/>
+        <?php smore_menu_nav(); ?>
 
-    <br />
-    <?php smore_menu_nav(); ?>
-
-  </nav>
+    </nav>
 
 </nav>
